fe7475d7c880c29dba281f56a198a5384d72d79f
Anwesha Saha <anwesha@bu.edu>
2025-01-31 03:16:23 -0500

    updated range query to print 0 time for empty

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ std::string kRangeQueriesPath = "./range_queries.txt";
 const uint32_t kRuns = 3;
 using namespace std;
 
-void
-loadPointQueries(std::string &input_queries_path, std::vector<int> &queries) {
+void loadPointQueries(std::string &input_queries_path, std::vector<int> &queries) {
     queries.clear();
     std::ifstream infile(input_queries_path, ios::in);
     int tmp;
@@ -32,9 +31,7 @@ loadPointQueries(std::string &input_queries_path, std::vector<int> &queries) {
     // std::mt19937{std::random_device{}()});
 }
 
-void
-loadRangeQueries(std::string &input_queries_path,
-                 std::vector<std::pair<int, int>> &queries) {
+void loadRangeQueries(std::string &input_queries_path, std::vector<std::pair<int, int>> &queries) {
     queries.clear();
     std::ifstream infile(input_queries_path, ios::in);
     // in the range_queries.txt file, there are two values instead of one
@@ -57,9 +54,7 @@ loadRangeQueries(std::string &input_queries_path,
     // std::mt19937{std::random_device{}()});
 }
 
-int
-main(int argc, char **argv) {
-
+int main(int argc, char **argv) {
     // read data
     std::ifstream ifs;
     std::vector<int> data;
@@ -108,26 +103,32 @@ main(int argc, char **argv) {
     // this part is exactly the same as point queries since the only difference
     // is that range queries are a pair of values
     loadRangeQueries(kRangeQueriesPath, range_queries);
-    auto start_rq = std::chrono::high_resolution_clock::now();
-    for (size_t r = 0; r < kRuns; r++) {
-        for (const auto &rq : range_queries) {
-            // we need to call the appropriate query function defined in
-            // zonemaps
-            zones.query(rq.first, rq.second);
-        }
-        // shuffle indexes
-        // uncomment the shuffle used for the required version of cpp.
-        // random_shuffle no longer works
-        // std::shuffle(range_queries.begin(), range_queries.end(),
-        // std::mt19937{std::random_device{}()});
-        std::random_shuffle(queries.begin(), queries.end());
+    //if there is no range query there is no point in doing this
+    if(!range_queries.empty()){
+      auto start_rq = std::chrono::high_resolution_clock::now();
+      for (size_t r = 0; r < kRuns; r++) {
+          for (const auto &rq : range_queries) {
+              // we need to call the appropriate query function defined in
+              // zonemaps
+              zones.query(rq.first, rq.second);
+          }
+          // shuffle indexes
+          // uncomment the shuffle used for the required version of cpp.
+          // random_shuffle no longer works
+          // std::shuffle(range_queries.begin(), range_queries.end(),
+          // std::mt19937{std::random_device{}()});
+          std::random_shuffle(queries.begin(), queries.end());
+      }
+      auto stop_rq = std::chrono::high_resolution_clock::now();
+      // difference between start and end - same as point queries
+      auto duration_rq = std::chrono::duration_cast<std::chrono::microseconds>(
+          stop_rq - start_rq);
+      unsigned long long range_query_time = duration_rq.count();
+      std::cout << "Time taken to perform range query from zonemap = "
+                << range_query_time * 1.0 / kRuns << " microseconds" << endl;
     }
-    auto stop_rq = std::chrono::high_resolution_clock::now();
-    // difference between start and end - same as point queries
-    auto duration_rq = std::chrono::duration_cast<std::chrono::microseconds>(
-        stop_rq - start_rq);
-    unsigned long long range_query_time = duration_rq.count();
-    std::cout << "Time taken to perform range query from zonemap = "
-              << range_query_time * 1.0 / kRuns << " microseconds" << endl;
-    return 0;
+    else{
+      std::cout << "Time taken to perform range query from zonemap = 0 microseconds" << endl;
+    }
+    return 0; 
 }
